c538b2261ca8f29c71df38984b65a167db6ac03f
shanunath29 <158049576+shanunath29@users.noreply.github.com>
2024-01-29 17:59:57 +0530

    Add files via upload

---
+++ details.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<<"What is your name? ";
+    cin>>name;
+    cout<<"My name is: - "<<name<<endl;
+
+    int age;
+    cout<<"What is your age? ";
+    cin>>age;
+    cout<<"My age is: - "<<age<<endl;
+
+    char gender;
+    cout<<"What is your gender? ";
+    cin>>gender;
+    cout<<"My weight is: - "<<gender<<endl;
+
+    int weight;
+    cout<<"What is your weight? ";
+    cin>>weight;
+    cout<<"My weight is: - "<<weight<<endl;  
+
+
+    return 0;
+}
+
