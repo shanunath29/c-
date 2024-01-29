c07b3d88585f4669ede369fa88b82a7ba69e1fff
shanunath29 <158049576+shanunath29@users.noreply.github.com>
2024-01-29 17:35:33 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //integer value
+    int integerValue = 20;
+    cout<<"Stored integer value is: "<<integerValue<<endl;
+
+    // Floating point datatype
+    float floatValue = 3.14;
+    cout<<"stored float value is: "<<floatValue<<endl;
+
+    // Character datatypes
+    char charValue = 'S';
+    cout<<"Stored character value is: "<<charValue<<endl;
+
+    // Boolean datatype
+    bool boolValue = false;
+    cout<<"Stored boolean value is: "<<boolValue<<endl;
+
+    return 0;
+}
